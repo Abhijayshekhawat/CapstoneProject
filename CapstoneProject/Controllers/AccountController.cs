97374f399f80f097cab9680ff003defc8efc1a89
Carlos Chavez <tug41350@temple.edu>
2024-09-16 12:19:29 -0400

    Create Account page front end

---
+++ CapstoneProject/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CapstoneProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateAccount()
+        {
+            return View();
+        }
+    }
+}
