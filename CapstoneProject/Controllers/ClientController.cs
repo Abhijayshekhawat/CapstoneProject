726c608fa038b91458d9bf116dfcb50e79242ff0
Carlos Chavez <tug41350@temple.edu>
2024-09-30 12:57:56 -0400

    edit client profile front end

---
+++ CapstoneProject/Controllers/ClientController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+//should be used for clients to edit their profile
+namespace CapstoneProject.Controllers
+{
+    public class ClientController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ClientProfile()
+        {
+            return View();
+        }
+
+        public IActionResult ClientProfileEdit()
+        {
+            return View();
+        }
+
+    }
+}
