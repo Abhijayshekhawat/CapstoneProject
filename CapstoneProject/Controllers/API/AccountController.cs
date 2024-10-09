52c87565a669441c058b17253dda8918f4fee11e
Abhijayshekhawat <abhijay.shekhawat@gmail.com>
2024-10-09 11:46:52 -0400

    added connection and account controller to project.

---
+++ CapstoneProject/Controllers/API/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CapstoneClassLibrary;
+
+namespace CapstoneProject.Controllers.API
+{
+    [Route("api/Account")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost("Login")]
+
+        public bool Login([FromBody] User user)
+        {
+            return false;
+            //bool result;
+            //User privateinfo = user;
+
+            //int Login = user.Login(user.Email, user.PasswordHash);
+
+            //if (Login > 0)
+            //{
+            //    result = true;
+            //}
+            //else { result = false; }
+            //return result;
+
+
+
+        }
+    }
+}
