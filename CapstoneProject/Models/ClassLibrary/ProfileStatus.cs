e8c1ada562e6bf17944094ee3e0ec6bdece6c80f
Johnny Tran <tun45633@temple.edu>
2024-11-06 18:21:31 -0500

    Approve/Reject Profiles is working, all profiles show "No comments" until status is changed for the first time.

--- CapstoneProject/Models/ClassLibrary/ProfileStatus.cs
+++ CapstoneProject/Models/ClassLibrary/ProfileStatus.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using CapstoneProject.Models.Utilities;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static CapstoneProject.Models.AdminModel;
 
 namespace CapstoneProject.Models.ClassLibrary
 {
@@ -33,15 +37,62 @@ namespace CapstoneProject.Models.ClassLibrary
 
 		}
 
-		public void UpdateProfileStatus()
+		public void UpdateProfileStatus(int profileID, int status, string comment)
 		{
+            using (Connection objDB = new Connection())
+            {
+                // Open the connection
+                if (!objDB.Open())
+                {
+                    // Handle the case where the connection couldn't be opened
+                    throw new Exception("Could not open database connection.");
+                }
 
-		}
+                // Create a SqlCommand object
+                SqlCommand objCommand = new SqlCommand
+                {
+                    CommandType = CommandType.StoredProcedure,
+                    CommandText = "AdminChangeProfileStatus"
+                };
 
-		public void AddCommentToProfileStatus()
+                // Add parameters to the command
+                SqlParameter inputParameter = new SqlParameter("@ProfileID", profileID);
+                objCommand.Parameters.Add(inputParameter);
+
+                // Add parameters to the command
+                SqlParameter inputParameter2 = new SqlParameter("@Status", status);
+                objCommand.Parameters.Add(inputParameter2);
+
+                // Add parameters to the command
+                SqlParameter inputParameter3 = new SqlParameter("@Comment", comment);
+                objCommand.Parameters.Add(inputParameter3);
+
+                objDB.DoUpdateUsingCmdObj(objCommand);
+            }
+        }
+
+		public void AddProfileComment(int profileID, int status, string comment)
 		{
+            using (Connection objDB = new Connection())
+            {
+                if (!objDB.Open())
+                {
+                    throw new Exception("Could not open database connection.");
+                }
 
-		}
+                SqlCommand objCommand = new SqlCommand
+                {
+                    CommandType = CommandType.StoredProcedure,
+                    CommandText = "AddProfileComment"
+                };
+
+                objCommand.Parameters.AddWithValue("@ProfileID", profileID);
+                objCommand.Parameters.AddWithValue("@Comment", comment);
+                objCommand.Parameters.AddWithValue("@Status", status);
+
+                objDB.DoUpdateUsingCmdObj(objCommand);
+            }
+        }
 
 
 	}
