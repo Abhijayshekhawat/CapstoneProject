5dd87b8571a9b63036a6c2e2ccd0c9f6a76c0ce2
Johnny Tran <tun45633@temple.edu>
2024-11-13 15:44:42 -0500

    Admin view profile now shows the user's usertype. AdminGetProfileData modified to include usertype.

--- CapstoneProject/Controllers/Admin/AdminProfilesController.cs
+++ CapstoneProject/Controllers/Admin/AdminProfilesController.cs
@@ -140,6 +140,20 @@ namespace CapstoneProject.Controllers.Admin
                         viewedProfile.Status = "Rejected";
                     }
 
+                    int uType = Convert.ToInt32(row["UserType"]); //user type is stored as an int in db, for the admin view we want to show the string
+                    if (uType == 1)
+                    {
+                        viewedProfile.UserType = "Client";
+                    }
+                    else if (uType == 2)
+                    {
+                        viewedProfile.UserType = "Reviewer";
+                    }
+                    else
+                    {
+                        viewedProfile.UserType = "Admin";
+                    }
+
                     //Get Profile Comments
                     Comment c = new Comment();
                     DataSet ds2 = new DataSet();
